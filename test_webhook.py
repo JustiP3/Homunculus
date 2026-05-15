33fff239b97e57c3ec800427354e1d1250f8638d
Justin <31192445+JustiP3@users.noreply.github.com>
2026-05-15 09:50:41 -0700

    Delete test_webhook.py

--- test_webhook.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-WEBHOOK_URL = "https://discord.com/api/webhooks/1502885789233844314/AmW3KK2l4A7jsOpq2SRKED-L-PfjHOm4vBhVKmLSS9wVLDO48-yPX8mjaEmLjwulr3iW"
-
-
-data = {"content": "hello friend!"}
-
-requests.post(WEBHOOK_URL, json=data)
