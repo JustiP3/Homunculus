abd4c55ca8129e11cbf8917e328f844f24757550
Justin <justinphil02@gmail.com>
2026-05-16 17:40:33 -0700

    add flask server dashboard framework

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+import db
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Flask server is working!"
+
+    return render_template("index.html")
+
+
+@app.route("/positions")
+def positions():
+
+    positions = db.get_open_positions()
+
+    return render_template(
+        "positions.html",
+        positions=positions
+    )
+
+
+@app.route("/watchlist")
+def watchlist():
+
+    symbols = db.get_watchlist()
+
+    return render_template(
+        "watchlist.html",
+        symbols=symbols
+    )
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    )
